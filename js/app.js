edac1ff8780560ed9ee38267d996a2bf6a2ef06c
wanghl <15761674230@163.com>
2018-09-22 00:05:13 +0800

    依赖多写了一个

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-define(['jquery', 'angular', 'ngRoute'], function ($, angular) {
+define(['jquery', 'angular'], function ($, angular) {
   var app = angular.module('myApp', ['ngRoute']); //引入需要以来的模块
   return app;
 });
